4f8d48192322848437ac38d8408e507b5f109ea7
OlegSotnik <oleg-00@yandex.ru>
2021-05-21 00:56:00 +0400

    внес изменения в всплывающее меню

---
+++ source/js/main-nav.js
@@ -0,0 +1,9 @@
+const mainNav = document.querySelector(".main-nav");
+const pageHeaderTop = document.querySelector(".page-header__top");
+const logoLink = document.querySelector(".logo-link");
+const pageHeader = document.querySelector(".page-header");
+
+mainNav.classList.add("main-nav--closed");
+pageHeaderTop.classList.add("page-header__top--background-color");
+logoLink.classList.add("logo-image--toggle-closed");
+pageHeader.classList.add("page-header--background");
