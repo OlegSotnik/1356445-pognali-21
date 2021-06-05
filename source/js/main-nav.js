6961bc50e90c5f5d8dba35ef8f5e4f6c60452eb2
OlegSotnik <oleg-00@yandex.ru>
2021-06-05 10:43:39 +0400

    сделал шапку сайта для всех версий

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -3,10 +3,9 @@ const pageHeaderTop = document.querySelector(".page-header__top");
 const logoLink = document.querySelector(".logo-link");
 const pageHeaderContainer = document.querySelector(".page-header-container");
 const mainNavToggle = document.querySelector(".main-nav__toggle");
+const buttonAuthorization = document.querySelector(".button-authorization");
 
 mainNav.classList.add("main-nav--closed");
-pageHeaderTop.classList.add("page-header__top--background-color");
-logoLink.classList.add("logo-image--toggle-closed");
 mainNavToggle.classList.remove("visually-hidden");
 
 mainNavToggle.addEventListener("click", function (evt) {
@@ -16,4 +15,5 @@ mainNavToggle.addEventListener("click", function (evt) {
   pageHeaderContainer.classList.toggle("page-header-container--background");
   logoLink.classList.toggle("logo-image--toggle-closed");
   mainNav.classList.toggle("main-nav--closed");
+  buttonAuthorization.classList.toggle("button-authorization--width-tablet-nav-open");
 });
